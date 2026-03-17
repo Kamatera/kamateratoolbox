4c87b0e58bb4a95d14c96a0d3b83d606fb8d40db
Ori Hoch <ori@uumpa.com>
2026-03-17 21:06:04 +0200

    add server create script directly to kamatera api using curl

---
+++ tests/kamatera_api/server_create.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+curl \
+  -XPOST \
+  -H "AuthClientId: ${KAMATERA_API_CLIENT_ID}" \
+  -H "AuthSecret: ${KAMATERA_API_SECRET}" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -d '{
+    "datacenter": "'${SERVER_DATACENTER}'",
+    "nServers": 1,
+    "names": ["'${SERVER_NAME}'"],
+    "cpuStr": "2B",
+    "cpuType": "B",
+    "ramMB": "2048",
+    "diskSizesGB": ["50"],
+    "password": "'${SERVER_PASSWORD}'",
+    "passwordValidate": "'${SERVER_PASSWORD}'",
+    "managed": false,
+    "backup": false,
+    "billingMode": 1,
+    "trafficPackage": "",
+    "useSimpleNetworking": false,
+    "powerOnCompletion": true,
+    "useSimpleWan": false,
+    "useSimpleLan": false,
+    "netModes": ["wan"],
+    "netNames": ["auto"],
+    "netSubnets": [""],
+    "netPrefixes": [0],
+    "netIps": ["auto"],
+    "diskImageId": "'${SERVER_IMAGE}'",
+    "sourceServerId": "",
+    "userId": 0,
+    "ownerId": 0,
+    "srcUI": false,
+    "selectedKey": null,
+    "script": "",
+    "selectedSSHKeyValue": "",
+    "selectedTags": [],
+    "userData": ""
+  }' "https://console.kamatera.com/svc/serverCreate"
